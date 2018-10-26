3277b16a05cbbc88132bb3d30f932336cb583559
lovesky <aidarkhanov.a@chocomart.kz>
2018-10-26 15:41:35 +0600

    fixed bug of slider

--- app/scripts/product.js
+++ app/scripts/product.js
@@ -1,10 +1,6 @@
 var swiper = new Swiper('.swiper-product', {
     // centeredSlides: true,
     loop: true,
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false
-    },
     simulateTouch: false,
     navigation: {
         nextEl: '.swiper-product-next',
