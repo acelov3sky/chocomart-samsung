964fd10f9d39e6b0befa59f6c5582b797ed978af
lovesky <aidarkhanov.a@chocomart.kz>
2018-10-29 10:24:59 +0600

    added animation for sidebar appearance.

--- app/scripts/sidebar.js
+++ app/scripts/sidebar.js
@@ -1,12 +1,14 @@
 $(document).ready(function(){
     $('.toggle').click(function(){
         if($(this).children('div').hasClass('parameter-triangle--opened')) {
-            $(this).siblings('div').addClass('parameter-content-wrapper--closed')
+            $(this).siblings('div').addClass('parameter-content-wrapper--closed');
+            $(this).siblings('div').removeClass('parameter-content-wrapper--opened');
             $(this).children('div').removeClass('parameter-triangle--opened');
             $(this).children('h5').removeClass('title--active');
         }
         else{
-            $(this).siblings('div').removeClass('parameter-content-wrapper--closed')
+            $(this).siblings('div').removeClass('parameter-content-wrapper--closed');
+            $(this).siblings('div').addClass('parameter-content-wrapper--opened');
             $(this).children('div').addClass('parameter-triangle--opened');
             $(this).children('h5').addClass('title--active');
         }
