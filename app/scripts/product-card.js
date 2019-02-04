69f7d74106b1e4fc901f3fe009c1f78ff123a11e
BaRon23 <seidazymov.b@chocomart.kz>
2019-02-04 16:52:41 +0600

    MObile version of product card

--- app/scripts/product-card.js
+++ app/scripts/product-card.js
@@ -1,1 +1,13 @@
-
+var product_vard = new Vue({
+    el: '#product-mobile',
+    data () {
+        return {
+            char: false,
+            buy: false,
+            sent: false,
+            warrant: false
+        }
+    },
+    methods: {
+    }
+});
