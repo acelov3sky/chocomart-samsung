994713a2ae669c7e308bd456d9006b344c6f06eb
lovesky <aidarkhanov.a@chocomart.kz>
2019-04-10 17:04:33 +0600

    added many changes.

--- app/scripts/products.js
+++ app/scripts/products.js
@@ -1,8 +1,10 @@
-var vm = new Vue({
-    el: '#v-products',
-    methods: {
-        openSidebar () {
-            document.getElementById('v-sidebar-wrapper').classList.toggle('open-v-sidebar');
+window.onload = function() {
+    var vm = new Vue({
+        el: '#v-products',
+        methods: {
+            openSidebar () {
+                document.getElementById('v-sidebar-wrapper').classList.toggle('open-v-sidebar');
+            }
         }
-    }
-});
+    });
+}
