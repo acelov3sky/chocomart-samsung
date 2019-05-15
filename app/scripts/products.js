bfb5c4bf966d4c8c244f9cca6c963e627ad0521a
BaRon <baurzhan9706@gmail.com>
2019-05-15 16:32:28 +0600

    updated for registration and login

--- app/scripts/products.js
+++ app/scripts/products.js
@@ -8,3 +8,36 @@ if (document.getElementById('v-products') !== null) {
         }
     });
 }
+import axios from 'axios';
+if (document.getElementById('user_info') !== null) {
+    var vm = new Vue({
+        delimiters: ['~{', '}'],
+        el: '#user_info',
+        data() {
+          return {
+              loading: false,
+              done: false,
+              panding: false,
+          }
+        },
+        methods: {
+            reg() {
+                document.getElementById('registration-sign').style.display = "block";
+            },
+            logout () {
+                this.panding = true;
+                this.loading = true;
+                axios.post('/logout').then(res => {
+                    this.loading = false;
+                    this.done = true;
+                    setTimeout(() => {
+                        this.panding = false;
+                        this.done = false;
+                        location.reload();
+                    }, 1000);
+
+                })
+            }
+        }
+    });
+}
